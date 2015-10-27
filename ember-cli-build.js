5e1b5d56a2ffa75460601efb8949e8a37a8851ba
Kevin Pfefferle <kevin@pfefferle.co>
2015-10-27 18:57:07 -0400

    Install Framework7 CSS

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -18,6 +18,8 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('bower_components/Framework7/dist/css/framework7.ios.min.css');
+  app.import('bower_components/Framework7/dist/css/framework7.ios.colors.min.css');
 
   return app.toTree();
 };
