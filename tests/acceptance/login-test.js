b914e7ec80300e76de8e0aeed2f3d71965fb727a
Kevin Pfefferle <kevin@pfefferle.co>
2015-10-30 21:42:41 -0400

    Redirect unauthenticated to login

---
+++ tests/acceptance/login-test.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from 'gitworm/tests/helpers/start-app';
+
+module('Acceptance | login', {
+  beforeEach: function() {
+    this.application = startApp();
+  },
+
+  afterEach: function() {
+    Ember.run(this.application, 'destroy');
+  }
+});
+
+test('visiting / when logged out should redirect to /login', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/login');
+  });
+});
