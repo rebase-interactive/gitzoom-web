30b93d36a8e487602f3c47d1329ae84ce04b5d0f
Kevin Pfefferle <kevin@pfefferle.co>
2015-11-09 17:13:00 -0500

    Update dependencies per pending ember-cli update

--- config/deploy.js
+++ config/deploy.js
@@ -14,7 +14,8 @@ module.exports = function(deployTarget) {
     cloudfront: {
       accessKeyId: process.env.AWS_KEY,
       secretAccessKey: process.env.AWS_SECRET,
-      distributionId: process.env.AWS_DISTRIBUTION_ID
+      distributionId: process.env.AWS_DISTRIBUTION,
+      region: process.env.AWS_REGION
     }
   };
 
