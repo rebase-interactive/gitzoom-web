9346c8eae61ddf7708d8096ae15094b0a9c00ecb
Kevin Pfefferle <kevin@pfefferle.co>
2015-11-17 18:17:11 -0500

    Open GitHub auth when sign in button is clicked

--- app/login/controller.js
+++ app/login/controller.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  session: Ember.inject.service('session'),
+
+  actions: {
+    authenticate() {
+      this.get('session').authenticate('authenticator:torii', 'github-oauth2');
+    }
+  }
 });
