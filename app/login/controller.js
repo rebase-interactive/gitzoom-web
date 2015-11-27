87fdbf9c242d01be4a3a1e86fed65bad8adc3d3e
Kevin Pfefferle <kevin@pfefferle.co>
2015-11-27 17:54:39 -0500

    Replace sign in link with spinner after GH auth

--- app/login/controller.js
+++ app/login/controller.js
@@ -3,9 +3,17 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
 
+  isProcessing: false,
+
   actions: {
     authenticate() {
-      this.get('session').authenticate('authenticator:torii', 'github-oauth2');
+      this.get('session').authenticate('authenticator:torii', 'github-oauth2')
+        .then(() => {
+          this.set('isProcessing', true);
+        })
+        .catch(() => {
+          // Don't throw error openly if user closes auth window
+        });
     }
   }
 });
