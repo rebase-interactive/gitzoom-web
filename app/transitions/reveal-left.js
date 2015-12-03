e08238c274f6e94a6c0b420aaaf6e2ef5930bb4f
Kevin Pfefferle <kevin@pfefferle.co>
2015-12-03 17:57:13 -0500

    Extract reveal transitions to liquid-fire-reveal addon

--- app/transitions/reveal-left.js
+++
@@ -1,5 +0,0 @@
-import reveal from "./reveal";
-
-export default function(opts) {
-  return reveal.call(this, 'x', -1, opts);
-}
--- app/transitions/reveal-up.js
+++
@@ -1,5 +0,0 @@
-import reveal from "./reveal";
-
-export default function(opts) {
-  return reveal.call(this, 'y', -1, opts);
-}
