f6f1042614a5f094b2c2b9e70537b4733b566c64
Kevin Pfefferle <kevin@pfefferle.co>
2015-12-15 11:53:55 -0500

    Use Gatekeeper to exchange OAuth code for token

--- app/login/controller.js
+++ app/login/controller.js
@@ -9,21 +9,21 @@ export default Ember.Controller.extend({
 
   actions: {
     authenticate() {
-      this.get('session').authenticate('authenticator:torii', 'github-oauth2')
+      return this.get('session').authenticate('authenticator:torii', 'github-oauth2')
         .then(() => {
           this.set('isProcessing', true);
 
           const authCode = this.get('session.data.authenticated.authorizationCode');
           const gatekeeperURL =  'https://' + ENV.GATEKEEPER_HOST + '/authenticate/' + authCode;
-          console.log(gatekeeperURL);
 
-          // return this.get('ajax').request(gatekeeperURL)
-          //   .then((data) => {
-          //     console.log('Data: ', data);
-          //     console.log('Token: ', data.token);
-          //   }).catch((data) => {
-          //     console.log('Error Data: ', data);
-          //   });
+          return this.get('ajax').request(gatekeeperURL)
+            .then((data) => {
+              if (data.error) {
+                alert('Error: ' + data.error);
+              } else {
+                console.log('Token: ', data.token);
+              }
+            });
         })
         .catch(() => {
           // Don't throw console error if user dismisses auth window
