d2365c4c6ff8f9e3530e7faa666c6f3f686f664e
Kevin Pfefferle <kevin@pfefferle.co>
2015-12-16 11:23:06 -0500

    Add test for logged in redirect from /login to /

--- tests/acceptance/login-test.js
+++ tests/acceptance/login-test.js
@@ -1,12 +1,33 @@
-import { test } from 'qunit';
-import moduleForAcceptance from 'gitzoom/tests/helpers/module-for-acceptance';
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from 'gitzoom/tests/helpers/start-app';
+import { authenticateSession } from 'gitzoom/tests/helpers/ember-simple-auth';
 
-moduleForAcceptance('Acceptance | login');
+let application;
+
+module('Acceptance | login', {
+  beforeEach: function() {
+    application = startApp();
+  },
+
+  afterEach: function() {
+    Ember.run(application, 'destroy');
+  }
+});
 
 test('visiting / when logged out should redirect to /login', function(assert) {
   visit('/');
 
   andThen(function() {
     assert.equal(currentURL(), '/login');
+  });
+});
+
+test('visiting /login when logged in should redirect to /', function(assert) {
+  authenticateSession(application);
+  visit('/login');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/');
   });
 });
