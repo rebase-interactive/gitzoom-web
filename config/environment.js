7eb60a523ab1271f0913315738d64a1d477ad2ca
Kevin Pfefferle <kevin@pfefferle.co>
2015-12-27 15:30:09 -0500

    Show user avatar as logout link

--- config/environment.js
+++ config/environment.js
@@ -21,7 +21,7 @@ module.exports = function(environment) {
       'script-src': "'self'",
       'font-src': "'self'",
       'connect-src': `'self' ${process.env.GATEKEEPER_HOST} api.github.com`,
-      'img-src': "'self' data:",
+      'img-src': "'self' data: *.githubusercontent.com",
       'style-src': "'self'",
       'media-src': "'self'"
     },
