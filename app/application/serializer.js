a1437cb5ce4736b1b10bafcd7c16a85ef60c54fb
Kevin Pfefferle <kevin@pfefferle.co>
2016-01-01 01:08:35 -0500

    Use application serializer for more generic key conversion

--- app/application/serializer.js
+++ app/application/serializer.js
@@ -1,4 +1,8 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.JSONSerializer.extend({
+  keyForAttribute(attr) {
+    return Ember.String.underscore(attr);
+  }
 });
--- app/user/serializer.js
+++ app/user/serializer.js
@@ -3,14 +3,5 @@ import ApplicationSerializer from 'gitzoom/application/serializer';
 export default ApplicationSerializer.extend({
   extractId() {
     return 'me';
-  },
-  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-    /*jshint unused: false*/
-
-    payload.avatarUrl = payload.avatar_url;
-
-    delete payload.avatar_url;
-
-    return this._super(...arguments);
   }
 });
