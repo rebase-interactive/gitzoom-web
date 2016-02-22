4773c224d2f7a7854e715005029bd8de360b90ed
Kevin Pfefferle <kevin@pfefferle.co>
2016-02-21 21:26:47 -0500

    Move protected template content into protected index

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('login');
-  this.route('protected', {path: '/'});
+  this.route('protected', {path: '/'}, function() {});
 });
 
 export default Router;
