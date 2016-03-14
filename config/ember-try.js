5d0d034e50197f986933eac41a241d53c9d7a1f4
Kevin Pfefferle <kevin@pfefferle.co>
2016-03-13 22:08:11 -0400

    Add ember-try config for npm test

---
+++ config/ember-try.js
@@ -0,0 +1,33 @@
+/*jshint node:true*/
+module.exports = {
+  scenarios: [
+    {
+      name: 'default',
+      bower: {
+        dependencies: { }
+      }
+    },
+    {
+      name: 'ember-beta',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#beta'
+        },
+        resolutions: {
+          'ember': 'beta'
+        }
+      }
+    },
+    {
+      name: 'ember-canary',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#canary'
+        },
+        resolutions: {
+          'ember': 'canary'
+        }
+      }
+    }
+  ]
+};
