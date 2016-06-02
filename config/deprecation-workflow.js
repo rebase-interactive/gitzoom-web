f02863e3fe34db3860a7b67a97c8522c63da6009
Kevin Pfefferle <kevin@pfefferle.co>
2016-06-01 21:29:26 -0400

    Remove unneeded deprecation silencer

--- config/deprecation-workflow.js
+++
@@ -1,6 +0,0 @@
-window.deprecationWorkflow = window.deprecationWorkflow || {};
-window.deprecationWorkflow.config = {
-  workflow: [
-    { handler: "silence", matchMessage: "Usage of `Ember.merge` is deprecated, use `Ember.assign` instead." }
-  ]
-};
