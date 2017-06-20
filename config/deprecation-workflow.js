123e763e57db61976dd85d34dc4a68d4a486abd1
Kevin Pfefferle <kevin@pfefferle.co>
2017-06-20 13:32:42 -0400

    Update deprecation-workflow config

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,6 +1,6 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: "silence", matchId: "ember-router.router" }
+    { handler: "silence", matchId: "ember-metal.model_factory_injections" }
   ]
 };
