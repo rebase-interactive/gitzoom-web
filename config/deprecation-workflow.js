f6ea5a7a95fe0b6cae9ffe78cafde90fadad0627
Kevin Pfefferle <kevin@pfefferle.co>
2017-07-05 22:35:39 -0400

    Fix MODEL_FACTORY_INJECTIONS deprecation

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,6 +1,5 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    { handler: "silence", matchId: "ember-metal.model_factory_injections" }
   ]
 };
