6e93b6b24942a54e2e4fc178e0359b4f606f8332
Kevin Pfefferle <kevin@pfefferle.co>
2018-02-13 19:56:05 -0500

    Fix ember-cli-page-object import deprecations

--- tests/pages/index.js
+++ tests/pages/index.js
@@ -1,9 +1,4 @@
-import PageObject from 'gitzoom/tests/page-object';
-
-const {
-  create,
-  visitable
-} = PageObject;
+import { create, visitable } from "ember-cli-page-object";
 
 export default create({
   visit: visitable('/')
