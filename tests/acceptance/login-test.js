c338c4ff51b6f2b54638eea01f748b224163d0c9
Kevin Pfefferle <kevin@pfefferle.co>
2018-03-03 17:03:37 -0500

    ⬆️ ember-cli-mirage@0.4.2

--- tests/acceptance/login-test.js
+++ tests/acceptance/login-test.js
@@ -2,7 +2,7 @@
 import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
-import setupMirage from 'gitzoom/tests/helpers/ember-cli-mirage';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
 import { authenticateSession } from 'ember-simple-auth/test-support';
 
