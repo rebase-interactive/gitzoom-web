acf119283209d2f90b33b9a1d7e8121bb7ed5600
Kevin Pfefferle <kevin@pfefferle.co>
2018-07-05 16:23:44 -0400

    Updates for framework7@3.0.0

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -29,7 +29,7 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('node_modules/framework7/dist/css/framework7.ios.min.css');
+  app.import('node_modules/framework7/css/framework7.ios.min.css');
 
   return app.toTree();
 };
